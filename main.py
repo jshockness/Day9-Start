cbe22af2941d85883d98102a6f31dccc3b044a9d
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-16 08:35:15 +0000

    create, delete and edit content

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ programming_dictionary = {
 }
 
 # Retrieving items from the dictionary 
-# print(programming_dictionary["Function"])
+print(programming_dictionary["Function"])
 
 # Adding new items to dictionary
 programming_dictionary["Loop"] = "The action of doing something over and over again."
 print(programming_dictionary)
+
+# Create an empty dictionary
+empty_dictionary = {}
+
+# Delete contents of dictionary
+programming_dictionary = {}
+
+# Edit content in dictionary
+programming_dictionary["Bug"] = "A moth in the computer"
+
