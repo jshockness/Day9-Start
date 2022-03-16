45a96ac4e77d5edf5ba3560a246ed125aa437950
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-16 08:17:56 +0000

    retrieve and add to dictionary

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again."
+}
+
+# Retrieving items from the dictionary 
+# print(programming_dictionary["Function"])
+
+# Adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
